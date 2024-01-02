ad5334dbe8b92462719d112557d89948d8df4a72
davidscales7 <81639121+davidscales7@users.noreply.github.com>
2024-01-02 13:37:59 +0000

    Update Staff.java

--- Staff.java
+++ Staff.java
@@ -1,5 +1,64 @@
 package cw;
+import java.io.*;
+import java.util.*;
 
-public class Staff {
 
+public class Staff 
+
+{
+	private int PersonsID;
+	private int StaffNumber;
+	private String UserName;
+	private String Password;
+	private String StaffPosition;
+
+public Staff (int PID, int StaffNumbers,String UserNames ,String Passwords ,String Positions)
+{
+	PersonsID = PID;
+	StaffNumber = StaffNumbers;
+	UserName = UserNames;
+	Password = Passwords;
+	StaffPosition = Positions;
+	
+}
+public int getPersonsID() {
+    return PersonsID;
+}
+
+public int getStaffNumber() {
+    return StaffNumber;
+}
+
+public String getUserName() {
+    return UserName;
+}
+
+public String getPassword() {
+    return Password;
+}
+
+public String getStaffPosition() {
+    return StaffPosition;
+}
+
+// Setter methods
+public void setPersonsID(int personsID) {
+    PersonsID = personsID;
+}
+
+public void setStaffNumber(int staffNumber) {
+    StaffNumber = staffNumber;
+}
+
+public void setUserName(String userName) {
+    UserName = userName;
+}
+
+public void setPassword(String password) {
+    Password = password;
+}
+
+public void setStaffPosition(String staffPosition) {
+    StaffPosition = staffPosition;
+}
 }
