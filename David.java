ed5592874e0a94147b66e53bc281d5ef8b3f2c0f
davidscales7 <81639121+davidscales7@users.noreply.github.com>
2024-01-02 13:37:24 +0000

    Update David.java

--- David.java
+++ David.java
@@ -1,5 +1,110 @@
 package cw;
+import java.io.*;
+import java.util.*;
 
-public interface David {
+
+public class Competitor {
+	private Date DateOfBirth;  
+	private int CompetitionNumber ;
+	private int CompetitorNumber;
+	private int Age;
+	private String Category;
+	private String Email;
+	private int Level;
+	private String UserName;
+	private String Password;
+	
+public Competitor (Date DOB, int CompetitionN, int CompetitorN,int age, String cat,String Em,int Lev,String Un,String Pw)
+{
+	DateOfBirth = DOB;
+	CompetitionNumber = CompetitionN;
+	CompetitorNumber = CompetitorN;
+	Age = age;
+	Category = cat;
+	Email = Em;
+	Level = Lev;
+	UserName = Un;
+	Password = Pw;
+	}
+
+//get methods
+public Date getDateOfBirth() {
+	return DateOfBirth;
+	}
+public int getCompetitionNumber() {
+	return CompetitionNumber;
+	}
+
+public int getCompetitorNumber() {
+	return CompetitorNumber;
+	}
+public int getAge() {
+    return Age;
+}
+public double getOverallScore() { return 5; }
+getfulldetails.append(// all the score)
+}
+getShortdetails.append(//only competitor number, initials and overall score)
+		// looks exactly like CN100(KJT) has overall score 5.
+public String getCategory() {
+    return Category;
+}
+
+public String getEmail() {
+    return Email;
+}
+
+public int getLevel() {
+    return Level;
+}
+
+public String getUserName() {
+    return UserName;
+}
+
+public String getPassword() {
+    return Password;
+}
+
+
+// set methods
+public void setDateOfBirth(Date dateOfBirth) {
+    DateOfBirth = dateOfBirth;
+}
+
+public void setCompetitionNumber(int competitionNumber) {
+    CompetitionNumber = competitionNumber;
+}
+
+public void setCompetitorNumber(int competitorNumber) {
+    CompetitorNumber = competitorNumber;
+}
+
+public void setAge(int age) {
+    Age = age;
+}
+
+public void setCategory(String category) {
+    Category = category;
+}
+
+public void setEmail(String email) {
+    Email = email;
+}
+
+public void setLevel(int level) {
+    Level = level;
+}
+
+public void setUserName(String userName) {
+    UserName = userName;
+}
+
+public void setPassword(String password) {
+    Password = password;
+}
+
+
 
 }
+
